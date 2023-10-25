a67bbd1ae220ecbd2fa794f32c5d2f58b17e87fb
Albert Oliva Roura <albertolivaroura@Alberts-MacBook-Air.local>
2023-10-25 16:32:57 +0200

    logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      React
+    <div className='todo-list'>
+      <div className='my-logo-container'>
+        <img src={logo} className='my-logo' />
+      </div>
+      <div className='main-list-tasks'>
+        <h1>My tasks</h1>
+      </div>
     </div>
   );
 }
