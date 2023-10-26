411719cd126a6b6a41e00fcdfc974d40462e5d56
Albert Oliva Roura <albertolivaroura@Alberts-MacBook-Air.local>
2023-10-26 15:26:21 +0200

    add icon

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../style/Task.css'
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 function Task({ text }) {
     return (
@@ -8,7 +9,7 @@ function Task({ text }) {
                 {text}
             </div>
             <div className="icon-task">
-                ToBeSubstitute
+                <AiOutlineCloseCircle />
             </div>
         </div>
     );
