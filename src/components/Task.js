707bb84fff073a49be6f4ec63adaefc4243ec59b
Albert Oliva Roura <albertolivaroura@Alberts-MacBook-Air.local>
2023-10-26 22:36:05 +0200

    mark as complete or delete task

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,13 +2,13 @@ import React from "react";
 import '../style/Task.css'
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
-function Task({ text, completed }) {
+function Task({ id, text, completed, completTask, deleteTask }) {
     return (
         <div className={ completed ? 'container-task completed' : 'container-task'}>
-            <div className="task-text">
+            <div className="task-text" onClick={() => completTask(id)}>
                 {text}
             </div>
-            <div className="icon-task-container">
+            <div className="icon-task-container" onClick={() => deleteTask(id)}>
                 <AiOutlineCloseCircle className="icon-task" />
             </div>
         </div>
